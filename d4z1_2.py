f4dcd7c8f6d6de513dccc6d8f3940de1d604bd29
roloomp <kverzy111@gmail.com>
2025-12-22 21:05:44 +0900

    d4z1_2

--- d4z1_2.py
+++ d4z1_2.py
@@ -59,30 +59,34 @@ maze = [
 
 def solve(mz):
     dvij = {
-        "up": (0, -1),
-        "down": (0, 1),
-        "left": (-1, 0),
-        "right": (1, 0),
+        "up": (-1, 0),
+        "down": (1, 0),
+        "left": (0, -1),
+        "right": (0, 1),
     }
     shir = len(mz[0])
     visot = len(mz)
     for i in range(visot):
         for j in range(shir):
             if mz[i][j] == 'S':
-                pos = [i, j]
+                start_pos = [i, j]
     bili = []
-    def dvij(pos):
-        if pos[0] < 0 or pos[0] >= shir or pos[1] < 0 or pos[1] >= visot:
+    def move(pos):
+        x, y = pos[0], pos[1]
+        if x < 0 or x >= visot or y < 0 or y >= shir:
             return None
-        if mz[pos[i]][pos[i][j]] == '#' or pos in bili:
+        if mz[x][y] == '#' or pos in bili:
             return None
-        if mz[pos[i]][pos[i][j]] == 'X':
-
-
-        return dvij
-
-
-print(solve(maze))
+        if mz[x][y] == 'X':
+            return []
+        bili.append(pos)
+        for name, (move_x, move_y) in dvij.items():
+            new_pos = (x + move_x, y + move_y)
+            result = move(new_pos)
+            if result is not None:
+                return [name] + result
+    return move(start_pos)
+print(*solve(maze))
 
 
 
